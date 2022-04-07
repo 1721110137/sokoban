e7271106ed9dbe74684092d4253c34645599ffab
José Alfredo Sorcia Cuellar <97415101+1721110137@users.noreply.github.com>
2022-04-07 13:08:31 +0000

    personaje_meta, caja, espacio / meta, hacia la izquierda, ambos puntos finalizados

--- sokoban.py
+++ sokoban.py
@@ -185,13 +185,6 @@ class Sokoban:
       self.mapa[self.muneco_fila][self.muneco_columna- 2] = 6
       self.muneco_columna -= 1
 
-    #05 - Muñeco, caja, meta [4, 2, 0] <- [6, 0, 1]
-    elif self.mapa[self.muneco_fila][self.muneco_columna] == 0 and self.mapa[self.muneco_fila][self.muneco_columna - 1] == 2 and self.mapa[self.muneco_fila][self.muneco_columna - 2] == 4:
-      self.mapa[self.muneco_fila][self.muneco_columna] = 1
-      self.mapa[self.muneco_fila][self.muneco_columna - 1] = 0
-      self.mapa[self.muneco_fila][self.muneco_columna- 2] = 6
-      self.muneco_columna - = 1
-
     #06 - Muñeco, caja_meta, espacio [1, 6, 0] <- [2, 5, 1]
     elif self.mapa[self.muneco_fila][self.muneco_columna] == 0 and self.mapa[self.muneco_fila][self.muneco_columna - 1] == 6 and self.mapa[self.muneco_fila][self.muneco_columna - 2] == 1:
       self.mapa[self.muneco_fila][self.muneco_columna] = 1
@@ -199,14 +192,28 @@ class Sokoban:
       self.mapa[self.muneco_fila][self.muneco_columna - 2] = 2
       self.muneco_columna -= 1
 
-    #07 - Muñeco, caja_meta, meta [4, 6, 0] -> [6, 5, 1]
+    #07 - Muñeco, caja_meta, meta [4, 6, 0] <- [6, 5, 1]
     elif self.mapa[self.muneco_fila][self.muneco_columna] == 0 and self.mapa[self.muneco_fila][self.muneco_columna - 1] == 6 and self.mapa[self.muneco_fila][self.muneco_columna - 2] == 4:
       self.mapa[self.muneco_fila][self.muneco_columna] = 1
       self.mapa[self.muneco_fila][self.muneco_columna - 1] = 5
       self.mapa[self.muneco_fila][self.muneco_columna - 2] = 6
-      self.muneco_columna -= 1 
+      self.muneco_columna -= 1
 
-   
+    #08 - Muñeco_meta, caja, espacio [1, 2, 5] -> [2, 0, 4]
+    elif self.mapa[self.muneco_fila][self.muneco_columna] == 5 and self.mapa[self.muneco_fila][self.muneco_columna - 1] == 2 and self.mapa[self.muneco_fila][self.muneco_columna - 2] == 1:
+      self.mapa[self.muneco_fila][self.muneco_columna] = 4
+      self.mapa[self.muneco_fila][self.muneco_columna - 1] = 0
+      self.mapa[self.muneco_fila][self.muneco_columna - 2] = 2
+      self.muneco_columna -= 1
+
+    #09 - Muñeco_meta, caja, meta [4, 2, 5] -> [6, 0, 4]
+    elif self.mapa[self.muneco_fila][self.muneco_columna] == 5 and self.mapa[self.muneco_fila][self.muneco_columna - 1] == 2 and self.mapa[self.muneco_fila][self.muneco_columna - 2] == 4:
+      self.mapa[self.muneco_fila][self.muneco_columna] = 4
+      self.mapa[self.muneco_fila][self.muneco_columna - 1] = 0
+      self.mapa[self.muneco_fila][self.muneco_columna - 2] = 6
+      self.muneco_columna -= 1
+
+    
 
 
 
